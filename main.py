d48a131ebbcdf0339df215bcff574f084b7e553d
Glaz Egy <rei.izayoi.0804@gmail.com>
2018-10-18 21:56:30 +0900

    Nomal game

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import sys
 import wx
 import os
 
+WhiteToBlack = {'white': (1, 'black', 0),
+                'black': (0, 'white', 1)}
+
+plusTuple = (-11, -10, -9, -1, 1, 9, 10, 11)
+
 class Player:
     def __init__(self, color):
         self.Color = color
         self.GetPositon = []
-        self.CanPut = self.SerchCanput()
+        self.CanPut = self.SerchCanPut()
+        self.TempGet = []
     
-    def CanPut(self):
+    def SerchCanPut(self):
+        return 0
 
 class Field:
     def __init__(self, panel, player):
@@ -32,10 +39,61 @@ class Field:
     def GameInit(self, player):
         self.Button[27].SetBackgroundColour('white')
         self.Button[36].SetBackgroundColour('white')
-        player[0].GetPositon.append()
+        player[0].GetPositon.append(44)
+        player[0].GetPositon.append(55)
         self.Button[28].SetBackgroundColour('black')
         self.Button[35].SetBackgroundColour('black')
-    
+        player[1].GetPositon.append(45)
+        player[1].GetPositon.append(54)
+
+def ConversionField(num, FromField='OutRangeField'):
+    if 'OutRangeField' == FromField:
+        x = num//10 - 1
+        return (num - (11 + (2 * x)))
+    else:
+        print('kk')
+
+def ListinList(fromList, toList):
+    for x in toList:
+        fromList.append(x)
+
+def IsGetCheck(color, pos):
+    EnemyIs = WhiteToBlack[color][0]
+    MyIs = WhiteToBlack[color][2]
+    CanputFlag = False
+    if pos not in player[0].GetPositon and pos not in player[1].GetPositon:
+        for plus in plusTuple:
+            if pos+plus in player[EnemyIs].GetPositon:
+                GetBuffer = []
+                for x in range(1, 9):
+                    if pos+(plus*x) in field.FieldOut:
+                        break
+                    if pos+(plus*x) in player[MyIs].GetPositon:
+                        CanputFlag = True
+                        ListinList(player[MyIs].TempGet, GetBuffer)
+                        break
+                    GetBuffer.append(pos+(plus*x))
+                else:
+                    del GetBuffer
+    return CanputFlag
+
+def ChangeColor(color, pos):
+    global nowColor
+    EnemyIs = WhiteToBlack[color][0]
+    MyIs = WhiteToBlack[color][2]
+    player[MyIs].TempGet.append(pos)
+    for num in player[MyIs].TempGet:
+        if num in player[EnemyIs].GetPositon: player[EnemyIs].GetPositon.remove(num)
+        player[MyIs].GetPositon.append(num)
+        field.Button[ConversionField(num)].SetBackgroundColour(color)
+    player[MyIs].TempGet = []
+    nowColor = WhiteToBlack[color][1]
+
+def ButtonPush(event):
+    ID = event.GetId()
+    if ID < 100:
+        if IsGetCheck(nowColor, ID):
+            ChangeColor(nowColor, ID)
 
 if __name__=='__main__':
     app = wx.App()
@@ -43,6 +101,8 @@ if __name__=='__main__':
     frame = wx.Frame(None, -1, AppName, pos=(0, 0), size=(1200, 1000), style=wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.CLIP_CHILDREN | wx.MINIMIZE_BOX)
     panel = wx.Panel(frame, -1)
     player = [Player('white'), Player('black')]
-    field = Field(panel)
+    field = Field(panel, player)
+    nowColor = 'black'
+    frame.Bind(wx.EVT_BUTTON ,ButtonPush)
     frame.Show()
     app.MainLoop()
