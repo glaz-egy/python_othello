2f5983ae4b7c0ef1e80a7b51c35fdc917d1da4cf
Glaz Egy <rei.izayoi.0804@gmail.com>
2018-10-22 08:03:26 +0900

    organize

--- kk.py
+++ kk.py
@@ -1,11 +1,18 @@
 import wx, wx.media
 
+def playtimer(event):
+    timer.Stop()
+    mc.Play()
+
 app = wx.App()
-frame = wx.Frame(None, -1)
+frame = wx.Frame(None, -1,size=(1910, 1070), pos=(0, 0))
 mc = wx.media.MediaCtrl(frame)
 if not mc.Load('「あぁ～！フロア熱狂の音ォ ！！」.mp4'):
     print('ff')
-mc.Play()
-mc.ShowPlayerControls()
+#mc.Play()
+#mc.ShowPlayerControls()
+timer = wx.Timer(frame)
+frame.Bind(wx.EVT_TIMER, playtimer)
+timer.Start(1000)
 frame.Show()
 app.MainLoop()
